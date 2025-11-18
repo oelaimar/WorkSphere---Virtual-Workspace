f81ac537618487578dc4c7ddb30e7a80dcc2cca5
oelaimar <oussama.el.elaimar@gmail.com>
2025-11-18 16:46:59 +0100

    feat: Add modal functionality

--- screpts/scripts.js
+++ screpts/scripts.js
@@ -1,3 +1,10 @@
+//global elements declation
+const addModal = document.getElementById("addModal");
+const addEmployeeBtn = document.getElementById("addEmployeeBtn");
+const closeModalsBtn = document.querySelectorAll(".closeBtn");
+const selectionModal = document.getElementById("selectionModal");
+const profileModal = document.getElementById("profileModal");
+const canselFormBtn = document.getElementById("canselFormBtn");
 
 
 // Zone Configuration
@@ -16,4 +23,23 @@ let employeeId = 1;
 let experienceIdCounter = 2;
 let currentZoneForSelection = null;
 
+function openAddModal(){
+    addModal.classList.remove("hidden");
+}
 
+addEmployeeBtn.addEventListener("click", openAddModal);
+canselFormBtn.addEventListener("click", closeModals)
+
+
+
+function closeModals(){
+    addModal.classList.add("hidden");
+    selectionModal.classList.add("hidden");
+    profileModal.classList.add("hidden");
+}
+
+
+
+closeModalsBtn.forEach(e => {
+    e.addEventListener("click", closeModals);
+});
